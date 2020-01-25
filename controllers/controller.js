0d06767cae1114f983811bdceeb7cdf1902b0d99
atcanbolat <kangalcanbolat@gmail.com>
2020-01-24 21:20:45 -0800

    base files added

---
+++ controllers/controller.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var router = express.Router();
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  
+});
+
+router.post("", function(req, res) {
+
+});
+
+router.put("", function(req, res) {
+
+});
+
+// Export routes for server.js to use.
+module.exports = router;
